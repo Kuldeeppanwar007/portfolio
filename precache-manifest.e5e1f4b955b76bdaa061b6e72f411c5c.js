40c502984de2cbd126dfa3bd3b1802d4c6ff1966
kuldeep.panwar007 <kuldeeppanwar7697@gmail.com>
2021-10-26 11:09:21 +0530

    Updates

---
+++ precache-manifest.e5e1f4b955b76bdaa061b6e72f411c5c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48cdf226d822cc3443a22d2d12bac1b8",
+    "url": "/portfolio.kuldeeppanwar.co.in/index.html"
+  },
+  {
+    "revision": "f123ea11d9a391345585",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "0f34efd83a79b7e645d4",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/css/4.ea3dbbf2.chunk.css"
+  },
+  {
+    "revision": "9aed0005a3a35ad41e4d",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/css/main.7eae6437.chunk.css"
+  },
+  {
+    "revision": "07bfa798211a951a4482",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/js/2.0506c526.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/js/2.0506c526.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f123ea11d9a391345585",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/js/3.0c507bf9.chunk.js"
+  },
+  {
+    "revision": "0f34efd83a79b7e645d4",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/js/4.c10568b9.chunk.js"
+  },
+  {
+    "revision": "9aed0005a3a35ad41e4d",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/js/main.5d6d3d9b.chunk.js"
+  },
+  {
+    "revision": "f23b8d6c7f2fe6290028",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/js/runtime-main.ea29b8b6.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "7328ebed2b8d4570a5a4a1de9fb92aee",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/media/LaptopManage.7328ebed.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "b4470ad177602d0ea871265ec21adaa6",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/media/amazonClone.b4470ad1.jpg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "53b2ad717fb383dc83d8ec5662e35fa7",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/media/cpLogo.53b2ad71.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "5984fdbe8c6e5f6b591d3eb630f5ed1d",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/media/idCard.5984fdbe.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "84e573ca674418dec1f1fe0d112288d2",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/media/vidhyasagar.84e573ca.jpg"
+  },
+  {
+    "revision": "53015aad4cf940e4d341c556ac2d8014",
+    "url": "/portfolio.kuldeeppanwar.co.in/static/media/vodafoneLogo.53015aad.png"
+  }
+]);
