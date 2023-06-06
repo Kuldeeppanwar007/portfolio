a99bcd252cb7d319306a5c0cb10310bcb51311e1
kuldeep <kuldeeppanwar7697@gmail.com>
2023-06-06 10:52:01 +0530

    Updates

---
+++ precache-manifest.6c27ac53934639d1d06ca1508f5a4514.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e643e8acce039098694b80409bfb71cb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "977694244a2ba51964bd",
+    "url": "/portfolio/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "67a82e76a50cb16ca633",
+    "url": "/portfolio/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "a849bfd92305da69bbfa",
+    "url": "/portfolio/static/css/main.eec3faaa.chunk.css"
+  },
+  {
+    "revision": "b785f4d56b6fd362eb58",
+    "url": "/portfolio/static/js/2.5f1534f9.chunk.js"
+  },
+  {
+    "revision": "3d4186a67aba203694a5f10d101a5461",
+    "url": "/portfolio/static/js/2.5f1534f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "977694244a2ba51964bd",
+    "url": "/portfolio/static/js/3.b09966b0.chunk.js"
+  },
+  {
+    "revision": "67a82e76a50cb16ca633",
+    "url": "/portfolio/static/js/4.e8fc4de5.chunk.js"
+  },
+  {
+    "revision": "a849bfd92305da69bbfa",
+    "url": "/portfolio/static/js/main.6d378a1d.chunk.js"
+  },
+  {
+    "revision": "764f5821ebad31e76ff0",
+    "url": "/portfolio/static/js/runtime-main.29da3d09.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "7328ebed2b8d4570a5a4a1de9fb92aee",
+    "url": "/portfolio/static/media/LaptopManage.7328ebed.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "85ed4df8cc0a8ca85dde23dea6d35125",
+    "url": "/portfolio/static/media/adsprint.85ed4df8.png"
+  },
+  {
+    "revision": "f883d7f02f4e8e0d31e4c3019d7135d4",
+    "url": "/portfolio/static/media/bharosa.f883d7f0.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "69ac9d57db72c87bfaba6fbbdd0379f2",
+    "url": "/portfolio/static/media/eclipse.69ac9d57.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "5984fdbe8c6e5f6b591d3eb630f5ed1d",
+    "url": "/portfolio/static/media/idCard.5984fdbe.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "894ea88471df65779f8015a0cbe59cf0",
+    "url": "/portfolio/static/media/singaji.894ea884.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "84e573ca674418dec1f1fe0d112288d2",
+    "url": "/portfolio/static/media/vidhyasagar.84e573ca.jpg"
+  }
+]);
